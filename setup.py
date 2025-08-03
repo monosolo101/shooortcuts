0507790b0cc666103ed08a61ec3067f696014358
monosolo101 <monosolo1on1@gmail.com>
2025-08-04 02:49:50 +0800

    fix: upgrade pypi version to 1.0.1 to avoid upload conflict

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="shooortcuts",
-    version="1.0.0",
+    version="1.0.1",
     packages=find_packages(),
     install_requires=[
         "gitpython>=3.1.0",
